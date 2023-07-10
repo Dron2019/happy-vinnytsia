5e429e0c3fe894b44ccdba6642952e469117c95c
Dron2019 <dron_2019@ukr.net>
2023-07-10 10:57:45 +0300

    som2

--- src/assets/scripts/modules/gallery/getGallerySlider.js
+++ src/assets/scripts/modules/gallery/getGallerySlider.js
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 export function getGallerySlider(id) {
     if (document.documentElement.dataset.status === 'local') {
         return new Promise(function (resolve, reject) {
@@ -13,5 +15,9 @@ export function getGallerySlider(id) {
             }, 1000)
         })
     }
+    const fd = new FormData();
+    fd.append('action', 'singleGallery');
+    fd.append('id', id);
+    return axios.post('/wp-admin/admin-ajax.php', fd);
 }
 
